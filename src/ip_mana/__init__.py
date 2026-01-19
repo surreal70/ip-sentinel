19da6912533a78f2bfedba2ca760107bab61a601
surreal70 <andreas.huemmer@adminsend.de>
2026-01-19 04:53:27 +0100

    Bump version to 0.4.3

--- src/ip_mana/__init__.py
+++ src/ip_mana/__init__.py
@@ -5,6 +5,6 @@ A modular Python console application for comprehensive IP address
 intelligence gathering.
 """
 
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 __author__ = "IP-ManA Development Team"
 __license__ = "MIT"
